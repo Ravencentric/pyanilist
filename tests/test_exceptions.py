a6360c322cc9cd6d74558ad6565f9fe17ba25a0f
Raventric <78981416+Ravencentric@users.noreply.github.com>
2024-03-17 01:40:04 +0530

    fix: make retries

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -6,22 +6,24 @@ from pyanilist import (
     ValidationError,
 )
 
+anilist = Anilist(retries=1)
+
 
 def test_anilist_title_doesnt_exist() -> None:
     with pytest.raises(HTTPStatusError, match="Not Found."):
-        Anilist().search("Title does not exist", type=MediaType.MANGA)
+        anilist.search("Title does not exist", type=MediaType.MANGA)
 
 
 def test_anilist_bad_search_combo() -> None:
     with pytest.raises(HTTPStatusError, match="Not Found."):
-        Anilist().search("Attack on titan", season_year=1999)
+        anilist.search("Attack on titan", season_year=1999)
 
 
 def test_anilist_wrong_input_types() -> None:
     with pytest.raises(ValidationError):
-        Anilist().search(123456789, season_year="hello", type=True)  # type: ignore
+        anilist.search(123456789, season_year="hello", type=True)  # type: ignore
 
 
 def test_anilist_bad_id() -> None:
     with pytest.raises(HTTPStatusError, match="400 Bad Request"):
-        Anilist().get(9999999999)
+        anilist.get(9999999999)
