f6d0cb6dfbbfdc316f29fe27adc8fa209946f696
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2024-07-11 01:10:02 +0530

    chore(tests): more tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-from pyanilist import FuzzyDate
+from pyanilist import FuzzyDate, MediaTitle
 
 
 def test_fuzzy_date() -> None:
@@ -7,7 +7,24 @@ def test_fuzzy_date() -> None:
     assert FuzzyDate(year=2023).iso_format() == "2023"
     assert FuzzyDate().iso_format() == ""
 
-    assert FuzzyDate(year=2023, month=1, day=4).as_int() == 20230104
-    assert FuzzyDate(year=2023, month=1).as_int() == 20230100
-    assert FuzzyDate(year=2023).as_int() == 20230000
-    assert FuzzyDate().as_int() == 10000000
+    yyyymmdd = FuzzyDate(year=2023, month=1, day=4)
+    assert yyyymmdd.as_int() == int(yyyymmdd) == 20230104
+    assert yyyymmdd.iso_format() == str(yyyymmdd) == "2023-01-04"
+
+    yyyymm = FuzzyDate(year=2023, month=1)
+    assert yyyymm.as_int() == int(yyyymm) == 20230100
+    assert yyyymm.iso_format() == str(yyyymm) == "2023-01"
+
+    yyyy = FuzzyDate(year=2023)
+    assert yyyy.as_int() == int(yyyy) == 20230000
+    assert yyyy.iso_format() == str(yyyy) == "2023"
+
+    empty = FuzzyDate()
+    assert empty.as_int() == int(empty) == 10000000
+    assert empty.iso_format() == str(empty) == ""
+
+
+def test_media_title() -> None:
+    assert str(MediaTitle(romaji="romaji", english="english", native="native")) == "english"
+    assert str(MediaTitle(romaji="romaji", native="native")) == "romaji"
+    assert str(MediaTitle(native="native")) == "native"
