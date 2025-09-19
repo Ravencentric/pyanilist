0aa419d0751dc0677ec47b68e483e49f7b47c373
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2025-09-20 00:40:05 +0530

    fix: remove unused (de)serialization methods

--- src/pyanilist/_types.py
+++ src/pyanilist/_types.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 from datetime import datetime, timedelta
-from typing import TYPE_CHECKING, Any, NamedTuple, TypeAlias, TypedDict, TypeVar, Union
-
-if TYPE_CHECKING:
-    from typing_extensions import Self
+from typing import NamedTuple, TypeAlias, TypedDict, TypeVar, Union
 
 import msgspec
 
@@ -49,74 +46,6 @@ class Base(
     kw_only=True,
 ):
     """Base class for AniList data structures."""
-
-    @classmethod
-    def from_dict(cls, data: dict[str, Any], /) -> Self:
-        """
-        Create an instance of this class from a dictionary.
-
-        Parameters
-        ----------
-        data : dict[str, Any]
-            Dictionary representing the instance of this class.
-
-        Returns
-        -------
-        Self
-            An instance of this class.
-
-        """
-        return msgspec.convert(data, type=cls)
-
-    def to_dict(self) -> dict[str, Any]:
-        """
-        Serialize the instance of this class into a dictionary.
-
-        Returns
-        -------
-        dict[str, Any]
-            Dictionary representing the instance of this class.
-
-        """
-        return msgspec.to_builtins(self)  # type: ignore[no-any-return]
-
-    @classmethod
-    def from_json(cls, data: str | bytes, /) -> Self:
-        """
-        Create an instance of this class from JSON data.
-
-        Parameters
-        ----------
-        data : str | bytes
-            JSON data representing the instance of this class.
-
-        Returns
-        -------
-        Self
-            An instance of this class.
-
-        """
-        return msgspec.json.decode(data, type=cls)
-
-    def to_json(self, *, indent: int = 2) -> str:
-        """
-        Serialize the instance of this class into a JSON string.
-
-        Parameters
-        ----------
-        indent : int, optional
-            Number of spaces for indentation.
-            Set to 0 for a single line with spacing,
-            or negative to minimize size by removing extra whitespace.
-
-        Returns
-        -------
-        str
-            JSON string representing this class.
-
-        """
-        jsonified = msgspec.json.encode(self)
-        return msgspec.json.format(jsonified, indent=indent).decode()
 
 
 class MediaTitle(Base, frozen=True, kw_only=True):
--- tests/test_async_anilist.py
+++ tests/test_async_anilist.py
@@ -86,9 +86,6 @@ async def test_anilist_get_media(async_anilist_client: AsyncAniList) -> None:
     assert isinstance(media.updated_at, datetime.datetime)
     assert media.volumes is None
 
-    assert media.from_dict(media.to_dict()) == media
-    assert media.from_json(media.to_json()) == media
-
 
 @pytest.mark.vcr
 async def test_anilist_get_media_sorted_by_id_desc(async_anilist_client: AsyncAniList) -> None:
